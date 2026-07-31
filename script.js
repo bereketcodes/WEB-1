142f2840dae0e470665d3d9874c640374dac6e36
Bereket Michael <Bereketmichael351@gmail.com>
2026-07-30 19:04:40 -0700

    add dom-manipulation example

---
+++ script.js
@@ -0,0 +1,51 @@
+// console.log("hello world");
+// console.log(12);
+// console.log(typeof (true));
+// console.log(5/0);
+// console.log(12**12);
+
+// creating a variable
+
+// let e = 30;
+// let swap;
+// swap = a;
+// a = e;
+// e = swap;
+// console.log(e);
+
+// let x= 3;
+// let y= "ddac"
+// let z= "3";
+
+// console.log(x>y);
+// console.log(x<y);
+// console.log(x>=y);
+// console.log(x<=y);
+// console.log(x==z);
+// console.log(x===z);
+
+// let temp = prompt("enter the temperature");
+// if (temp == 25) {
+//     console.log("at room temperature");   
+// } 
+// else if (temp > 25) {
+//     console.log("above room temperature");
+// } 
+// else {
+//     console.log("below room temperature");
+// }   
+
+// let i = 0;
+// let condition = false;
+// let sum = 0;   
+// while (i < 100) {
+//     console.log("this is multiple of 3: " + i);
+//     i+= 3;
+    
+// }
+let name ="beneyas";
+let names = [];
+for (let i = -1; i <= name.length; i++) {
+  names +=name[i];
+}   
+console.log(names);
